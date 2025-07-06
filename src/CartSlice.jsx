b19f95e7ed463da9aedb333389cfddbc37d8ce20
H.Vasev <hristovasev999@gmail.com>
2025-07-06 17:17:31 +0100

    Implementing cartSlice

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const currItem = state.items.find((item) => item.name === name);
+      if (currItem) {
+        currItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+     state.items = state.items.filter((item) => item.name !== action.payload)
     },
     updateQuantity: (state, action) => {
+      const {name, quantity} = action.payload;
 
-    
+      const currItem = state.items.find((item) => item.name === name);
+
+      if(currItem){
+        currItem.quantity = quantity
+      }
     },
   },
 });
